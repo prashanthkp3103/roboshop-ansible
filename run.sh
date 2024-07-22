2caed2791765c101a89f7218539d213307ff504b
prasanthbangs2016 <prasanthbangs@gmail.com>
2024-07-22 12:42:14 +0530

    ansible

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 git pull
-ansible-playbook -i $1.{{ env }}.meppk.xyz, -e env=dev -e ansible_user=ec2-user -e ansible_password=DevOps321 main.yml -e role_name=$1
+ansible-playbook -i $1.dev.meppk.xyz, -e env=dev -e ansible_user=ec2-user -e ansible_password=DevOps321 main.yml -e role_name=$1
